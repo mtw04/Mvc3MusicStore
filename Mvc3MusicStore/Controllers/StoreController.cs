52bbbfa5b41180f48c4ab3cfd07adacffdc33222
mtw04 <mtw04b@gmail.com>
2013-08-19 20:28:22 -0700

    start of exercise 5

---
+++ Mvc3MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Mvc3MusicStore.Models;
+
+namespace Mvc3MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            List<Genre> genres = storeDB.Genres.ToList();
+
+            return this.View(genres);
+        }
+        //
+        // GET: /Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            // Retrieve Genre and its Associated Albums from database
+            Genre genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
+
+            return this.View(genreModel);
+        }
+        //
+        // GET: /Store/Details
+        public ActionResult Details(int id = 1)
+        {
+            Album album = storeDB.Albums.Find(id);
+            
+            return this.View(album);
+        }
+    }
+}
---
+++ Mvc3MusicStore/Models/MusicStoreEntities.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace Mvc3MusicStore.Models
+{
+    public class MusicStoreEntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
