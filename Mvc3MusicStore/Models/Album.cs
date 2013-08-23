2a191abc36699fd6a3b837214683e7457cf186a5
mtw04 <mtw04b@gmail.com>
2013-08-23 16:32:26 -0700

    start of exercise 7

--- Mvc3MusicStore/Models/Album.cs
+++ Mvc3MusicStore/Models/Album.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
 namespace Mvc3MusicStore.Models
 {
+    [Bind(Exclude = "AlbumId")]
     public class Album
     {
+        [ScaffoldColumn(false)]
         public int AlbumId { get; set; }
+
+        [DisplayName("Genre")]
         public int GenreId { get; set; }
+
+        [DisplayName("Artist")]
         public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "An Album Title is required")]
+        [StringLength(160, ErrorMessage = "Album Title cannot exceed 160 characters")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 100.00, ErrorMessage = "Price must be between 0.01 and 100.00")]
         public decimal Price { get; set; }
+
+        [DisplayName("Album Art URL")]
+        [StringLength(1024, ErrorMessage = "Album Art URL cannot exceed 1024 characters")]
         public string AlbumArtUrl { get; set; }
-        public Genre Genre { get; set; }
-        public Artist Artist { get; set; }
+        
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
     }
 }
