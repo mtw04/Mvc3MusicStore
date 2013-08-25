8f3eb761060b084ffb7c3734a08f5bd53f17aac5
mtw04 <mtw04b@gmail.com>
2013-08-25 16:09:21 -0700

    start of exercise 9

---
+++ Mvc3MusicStore/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Mvc3MusicStore.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int RecordId { get; set; }
+        public string CartId { get; set; }
+        public int AlbumId { get; set; }
+        public int Count { get; set; }
+        public DateTime DateCreated { get; set; }
+        public virtual Album Album { get; set; }
+    }
+}
---
+++ Mvc3MusicStore/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Mvc3MusicStore.Models;
+
+namespace Mvc3MusicStore.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
